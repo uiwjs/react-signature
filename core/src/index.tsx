92b8129c66033876a3a7c327171cf2695d158aaa
jaywcjlove <398188662@qq.com>
2023-11-24 06:14:28 +0800

    feat: add `readonly` props.

--- core/src/index.tsx
+++ core/src/index.tsx
@@ -2,21 +2,31 @@ import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react
 import { getStroke, type StrokeOptions } from 'perfect-freehand';
 import { getSvgPathFromStroke, getBoundingClientRect, getClinetXY, defaultOptions, defaultStyle } from './utils';
 
+export * from 'perfect-freehand';
 export * from './utils';
 
 export interface SignatureProps extends React.SVGProps<SVGSVGElement> {
   prefixCls?: string;
   options?: StrokeOptions;
+  readonly?: boolean;
   onPointer?: (points: number[][]) => void;
 }
 
-let points: number[][] = [];
-
 const Signature = forwardRef<SVGSVGElement, SignatureProps>((props, ref) => {
-  const { className, prefixCls = 'w-signature', style, onPointer, options, ...others } = props;
+  const {
+    className,
+    prefixCls = 'w-signature',
+    style,
+    readonly = false,
+    onPointer,
+    options,
+    children,
+    ...others
+  } = props;
   const cls = [className, prefixCls].filter(Boolean).join(' ');
   const $svg = useRef<SVGSVGElement>(null);
   const $path = useRef<SVGPathElement>();
+  const pointsRef = useRef<number[][]>();
 
   useImperativeHandle<SVGSVGElement | null, SVGSVGElement | null>(ref, () => $svg.current, [$svg.current]);
 
@@ -24,47 +34,45 @@ const Signature = forwardRef<SVGSVGElement, SignatureProps>((props, ref) => {
     const { offsetY, offsetX } = getBoundingClientRect($svg.current);
     const clientX = e.clientX || e.nativeEvent.clientX;
     const clientY = e.clientY || e.nativeEvent.clientY;
-    points = [[clientX - offsetX, clientY - offsetY]];
+    pointsRef.current = [[clientX - offsetX, clientY - offsetY]];
     const pathElm = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     $path.current = pathElm;
     $svg.current!.appendChild(pathElm);
-    // $svg.current?.remove()
   };
 
   const handlePointerMove = (e: PointerEvent) => {
     if ($path.current) {
       const { offsetY, offsetX } = getBoundingClientRect($svg.current);
       const { clientX, clientY } = getClinetXY(e);
-      points = [...points, [clientX - offsetX, clientY - offsetY]];
-      const stroke = getStroke(points, { ...defaultOptions, ...options });
+      pointsRef.current = [...pointsRef.current!, [clientX - offsetX, clientY - offsetY]];
+      const stroke = getStroke(pointsRef.current!, { ...defaultOptions, ...options });
       const pathData = getSvgPathFromStroke(stroke);
       $path.current?.setAttribute('d', pathData);
     }
   };
 
   const handlePointerUp = () => {
-    onPointer && onPointer(points);
-    points = [];
+    let result = pointsRef.current || [];
+    onPointer && onPointer(result);
     $path.current = undefined;
+    pointsRef.current = undefined;
   };
 
   useEffect(() => {
+    if (readonly) return;
     document.addEventListener('pointermove', handlePointerMove);
     document.addEventListener('pointerup', handlePointerUp);
     return () => {
+      if (readonly) return;
       document.removeEventListener('pointermove', handlePointerMove);
       document.removeEventListener('pointerup', handlePointerUp);
     };
   }, []);
 
   return (
-    <svg
-      {...others}
-      ref={$svg}
-      className={cls}
-      onPointerDown={handlePointerDown}
-      style={{ ...defaultStyle, ...style }}
-    />
+    <svg {...others} ref={$svg} className={cls} onPointerDown={handlePointerDown} style={{ ...defaultStyle, ...style }}>
+      {children}
+    </svg>
   );
 });
 
